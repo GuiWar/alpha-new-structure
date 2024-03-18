7c4afba70e9b5a22a099214bb080ffb19871a34a
Guilherme Guerra Martins <guilherme.guerra@genesis.global>
2024-03-18 12:12:16 +0000

    first commit

---
+++ client/src/routes/home/home.template.ts
@@ -0,0 +1,7 @@
+import { html } from '@microsoft/fast-element';
+import type { Home } from './home';
+
+export const HomeTemplate = html<Home>`
+  <!-- insert template code here -->
+  Welcome to Home
+`;
